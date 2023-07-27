88f89652d87a8b0ce1f561a48e82f62957fc2e3f
DanielGarridoLuna <draymonluna@gmail.com>
2023-07-27 08:26:50 -0600

    Navegacion tabs

--- App.js
+++ App.js
@@ -1,25 +1,54 @@
-import React from 'react';
-import {View, Text, Image, ScrollView, TextInput} from 'react-native';
+import * as React from 'react';
+import { Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-const App = () => {
+import SRMain from './Pantallas/SRMain';
+
+
+function HomeScreen() {
   return (
-    <ScrollView>
-      <Text>Some text</Text>
-      <View>
-        <Text>Some more text</Text>
-        <Image
-          source={{
-            uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
-          }}
-          style={{width: 200, height: 200}}
-        />
-      </View>
-     
-      <Image source={require('./assets/DHnegro.png')}
-      style={{width: 200, height: 200}}
-      />
-    </ScrollView>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center',  backgroundColor:'crimson' }}>
+      <Text>Home!</Text>
+    </View>
   );
-};
+}
 
-export default App;
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+
+function NewScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Nueva Pantalla</Text>
+    </View>
+  );
+}
+
+
+function MainScreen() {
+  return (
+    <SRMain/>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Principal" component={HomeScreen} />
+        <Tab.Screen name="Secundaria" component={SettingsScreen} />
+        <Tab.Screen name="Opciones" component={NewScreen} />
+        <Tab.Screen name="Main" component={MainScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ Pantallas/SRMain.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import {View, StyleSheet, TextInput} from 'react-native';
+
+
+
+const SRMain = () => {
+
+    const styles = StyleSheet.create({
+        Contenedor:{
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+            
+        },
+        input: {
+          height: 40,
+          margin: 12,
+          borderWidth: 1,
+          padding: 10,
+          color:`#dc143c`
+        },
+      });
+
+
+    return (
+        <View style={styles.Contenedor}>
+
+        <TextInput
+        style={styles.input}
+        placeholder="Producto"
+       
+        />
+
+        <TextInput
+        style={styles.input}
+        placeholder="Precio"
+       
+        />
+
+        <TextInput
+        style={styles.input}
+        placeholder="Existencia"
+      
+        />
+
+        <TextInput
+        style={styles.input}
+        placeholder="Categoria"
+        
+        />
+
+        </View>
+      )
+}
+
+export default SRMain
